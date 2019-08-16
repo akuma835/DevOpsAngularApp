afe6114b2b9f18b501d9c33a1e00a6bb540beca0
akuma835 <amit.df.kumar@capgemini.com>
2019-08-16 16:09:38 +0530

    Final Angular App

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OnboardingFormComponent } from './form/onboarding-form/onboarding-form.component';
+import { DialogService, MessageService } from 'primeng/api';
+
+const routes: Routes = [
+  {
+    path:"home", component: OnboardingFormComponent,
+  },
+  {path: '', component: OnboardingFormComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+ 
+})
+export class AppRoutingModule { }
